798d1ada54a962227ffe8f087d095cd3ca702ebc
Leonard Chew <leochew@gmail.com>
2017-09-01 14:42:41 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -13,8 +13,7 @@ def position_taken?(board, index)
     board[index] == "" ||
     board[index] == nil
     return false
-  elsif
-    board[index] == "X" ||
+  elsif board[index] == "X" ||
     board[index] == "O"
     return true
   end
