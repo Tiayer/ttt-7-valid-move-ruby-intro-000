fefe87bb9b8b2010a151f20ff9a97da473516600
Leonard Chew <leochew@gmail.com>
2017-09-01 13:52:10 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,12 +1,6 @@
 # code your #valid_move? method here
-def between?(number)
-   number.to_i >= 0 &&
-   number.to_i <= 8
-end
-
-def valid_move?
-  if between?
-    execute position_taken?
+def valid_move?(board, index)
+  
 end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
