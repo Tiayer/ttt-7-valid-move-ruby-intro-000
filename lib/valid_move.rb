5f5aae8eda3650949a558ed452d04f3a7c7ed660
Leonard Chew <leochew@gmail.com>
2017-09-01 13:20:29 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -7,9 +7,9 @@ def between?(number)
     false
   end
 end
-    
+
 def valid_move?
-  
+
 end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
