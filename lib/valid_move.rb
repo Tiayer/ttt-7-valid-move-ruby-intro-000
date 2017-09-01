da5bf072b44773c050a3a730f7fc3999e030acc0
Leonard Chew <leochew@gmail.com>
2017-09-01 14:21:44 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -2,7 +2,7 @@
 def valid_move?(board, index)
   if index.between?(0, 8) == true
     true
-  elsif position_take?(board, index) == true
+  elsif position_taken?(board, index) == true
     false
   end
 end
